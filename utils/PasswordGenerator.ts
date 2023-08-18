f1bc58e217f4b236d5e72b986c3f717dfc1d570d
Iliya Farhadi <i2007f2007@gmail.com>
2023-08-18 11:27:13 +0330

    asd

--- utils/PasswordGenerator.ts
+++ utils/PasswordGenerator.ts
@@ -5,6 +5,7 @@ export default function GenerateRandomPassword(characterCount: number) {
   let password = "";
 
   for (let i = 1; i <= characterCount; i++) {
+    
     const randomIndex = Math.floor(Math.random() * charset.length);
     password += charset[randomIndex];
   }
